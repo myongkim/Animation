9ff078427589417159ff2f68cfd57aa36dc71ee3
Isaac Kim <myong328@gmail.com>
2022-04-11 10:54:44 -0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Isaac Kim on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Isaac Kim on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
